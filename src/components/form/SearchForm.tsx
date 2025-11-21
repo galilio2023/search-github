6a1459d063452e7d1a121788a3764665dc418352
ibrahim galal <ibrahimgalal2011@gmail.com>
2025-11-21 03:50:59 +0200

    remove unused

--- src/components/form/SearchForm.tsx
+++ src/components/form/SearchForm.tsx
@@ -1,5 +1,5 @@
 import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
+// import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { useToast } from "@/hooks/use-toast";
 
